8bf22de7098e44092c5d10554c92569ff241f589
sadiaalam200023 <sadi15156@gmail.com>
2023-11-18 13:01:38 +0600

    TechnicalOfficer scene update
    fxids and handler given

--- Simulation_Internet_Service_Provider_ISP/src/SadiaAlamNetworkEngineer/NetworkEngineerMainSceneController.java
+++ Simulation_Internet_Service_Provider_ISP/src/SadiaAlamNetworkEngineer/NetworkEngineerMainSceneController.java
@@ -11,7 +11,7 @@ import javafx.fxml.Initializable;
 /**
  * FXML Controller class
  *
- * @author HP
+ * @author dell
  */
 public class NetworkEngineerMainSceneController implements Initializable {
 
--- Simulation_Internet_Service_Provider_ISP/src/SadiaAlamTechnicalOfficer/TechnicalOfficerMainSceneController.java
+++ Simulation_Internet_Service_Provider_ISP/src/SadiaAlamTechnicalOfficer/TechnicalOfficerMainSceneController.java
@@ -6,21 +6,55 @@ package SadiaAlamTechnicalOfficer;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 
-/**
- * FXML Controller class
- *
- * @author HP
- */
 public class TechnicalOfficerMainSceneController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    @FXML
+    private TableView<?> listofTasksAppointmentsTableview;
+    @FXML
+    private TableColumn<?, ?> listofTasksAppointmentscolom;
+
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+       
     }    
+
+    @FXML
+    private void deletefromListonselection(ActionEvent event) {
+    }
+
+    @FXML
+    private void viewInventoryoncickfieldtech(ActionEvent event) {
+    }
+
+    @FXML
+    private void viewManualistonclick(ActionEvent event) {
+    }
+
+    @FXML
+    private void generateFieldreportonclick(ActionEvent event) {
+    }
+
+    @FXML
+    private void updateMaintenanceonClick(ActionEvent event) {
+    }
+
+    @FXML
+    private void makeAppointmentonclick(ActionEvent event) {
+    }
+
+    @FXML
+    private void makenewTaskticket(ActionEvent event) {
+    }
+
+    @FXML
+    private void viewselected(ActionEvent event) {
+    }
     
 }
